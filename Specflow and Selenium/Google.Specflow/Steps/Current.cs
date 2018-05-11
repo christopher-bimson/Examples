3ed996655305199b88700ef511bc1b97ae729552
Chris Bimson <christopher-bimson>
2018-05-11 10:38:55 +0100

    While Current.Value is less ugly than ScenarioContext.Current.Get/Set, putting in a helper class obscures the use of ScenarioContext.Current, which is bad. Updated the comments to explain using ScenarioContext to pass data between steps.

--- Specflow and Selenium/Google.Specflow/Steps/Current.cs
+++ Specflow and Selenium/Google.Specflow/Steps/Current.cs
@@ -5,6 +5,10 @@ namespace Google.Specflow.Steps
     /// <remarks>
     /// Idea borrowed from https://stackoverflow.com/a/13971643.
     /// Makes simple use of Scenario Context much less ugly.
+    /// 
+    /// That said, if you want to reliably share data, objects etc between 
+    /// Steps in a single Scenario, Scenario Context is where you should put 
+    /// things.
     /// </remarks>
     internal static class Current<T> where T : class
     {
